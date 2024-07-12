2117851e5da95f9bfddff0e8d67b375a2ad61aaf
Bgip369 <102659350+Bgip369@users.noreply.github.com>
2024-07-12 16:04:49 +0700

    admincode

--- core/feoneclick/urls.py
+++ core/feoneclick/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
@@ -7,6 +8,7 @@ from django.conf import settings
 
 urlpatterns = [
     # Acount #######################################################
+    path('', lambda request: redirect('dashboard/', permanent=True)),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/', views.Profile, name='profile'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
